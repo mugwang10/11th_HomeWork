e26e1edf0801b36a5d46d2a3ac26fcb52f948450
mugwang10 <imbeurhkd@gmail.com>
2025-05-23 13:22:36 +0900

    DT 모델링 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -8,6 +8,9 @@ from sklearn.datasets import load_wine
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 
+# DT 모델
+from sklearn.tree import DecisionTreeClassifier
+
 import matplotlib.pyplot as plt
 
 wine = load_wine()
@@ -27,7 +30,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random
 ####### A 작업자 작업 수행 #######
 
 ''' 코드 작성 바랍니다 '''
+# 하이퍼 파라미터
+dt_params = {
+    "criterion" : ['gini', 'entropy'],
+    "max_depth" : [2,, 5],
+    "min_samples_split": [2, 10],
+    "min_samples_leaf": [1, 2, 4]
+}
 
+# 모델 정의
+dt_model = DecisionTreeClassifier(random_state=42)
+
+# GridSearchCV로 튜닝
+dt_grid_search = GridSearchCV(dt_model, dt_params, cv=5, scoring='accuracy')
+
+# 학습
+dt_grid_search.fit(X_train, y_train)
 
 
 ####### B 작업자 작업 수행 #######
