cf2c132ee4d357fd1424f04009ae2b61207f8787
mugwang10 <imbeurhkd@gmail.com>
2025-05-23 13:25:17 +0900

    하이퍼 파라미터 조정

--- 코드_공유.py
+++ 코드_공유.py
@@ -33,8 +33,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random
 # 하이퍼 파라미터
 dt_params = {
     "criterion" : ['gini', 'entropy'],
-    "max_depth" : [2, 5],
-    "min_samples_split": [2, 10],
+    "max_depth" : [2, 3, 4, 5],
+    "min_samples_split": [2, 5, 10],
     "min_samples_leaf": [1, 2, 4]
 }
 
